dbada38a9fb20fab56f77f8a19eaace7388e4b07
saaya-code <boualimohamed74@gmail.com>
2023-05-03 09:51:39 +0100

    added subsciption to vocie

--- commands/listen.js
+++ commands/listen.js
@@ -2,6 +2,7 @@ const {SlashCommandBuilder} = require('discord.js');
 const { joinVoiceChannel } = require('@discordjs/voice');
 const { getVoiceConnection } = require('@discordjs/voice');
 const { VoiceConnectionStatus } = require('@discordjs/voice');
+const fs = require("fs")
 
 const data = new SlashCommandBuilder()
     .setName('join')
@@ -15,20 +16,28 @@ module.exports = {
     async execute(interaction) {
         const channelName = interaction.options.getString('channel') || interaction.member.voice.channel.name;
         const channel = interaction.guild.channels.cache.find(channel => channel.name === channelName);
-        if (channel) {
-            const connected = joinVoiceChannel({
-                channelId: channel.id,
-                guildId: channel.guild.id,
-                adapterCreator: channel.guild.voiceAdapterCreator,
-            });
-
-            
-        } else {
-            await interaction.reply({content: `Channel ${channelName} not found`, ephemeral: true});
-        }
-        const connection = getVoiceConnection(channel.guild.id);
+        if (!channel) return interaction.reply({content: `I could not find the channel ${channelName}`, ephemeral: true});
+        const connection = joinVoiceChannel({
+            channelId: channel.id,
+            guildId: channel.guild.id,
+            adapterCreator: channel.guild.voiceAdapterCreator,
+            selfDeaf: false,
+        });
         connection.on(VoiceConnectionStatus.Ready, () => {
-            console.log('The connection has entered the Ready state - ready to play audio!');
+            console.log('Connection is ready!');
+            const user = interaction.user;
+            const subscription = connection.subscribe(user, {
+                end: {
+                  behavior: connection.SubscriptionBehavior.Stop,
+                  duration: 10000,
+                },
+              });
+              subscription.on("data", (data) => {
+                console.log(data);
+              });
+        });
+        connection.on(VoiceConnectionStatus.Disconnected, () => {
+            console.log('Connection disconnected!');
         });
 
     }
